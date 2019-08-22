3f1213a2e168fdf47527b8375151ae77fc42b82e
jkattukudiyil <jkattukudiyil@oanda.com>
2019-08-22 12:48:59 -0400

    trying to fix formatting

--- src/biobot.py
+++ src/biobot.py
@@ -52,7 +52,7 @@ def handle_command(command, channel):
     response = None
     # This is where you start to implement more commands!
     if command.startswith("help"):
-        response = "Possible commands are: " + " ".join(command_list)
+        response = "Possible commands are: " + "\n".join(command_list)
 
     # Sends the response back to the channel
     slack_client.api_call(
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         with_team_state=False,
         auto_reconnect=True
     ):
-        print("Starter Bot connected and running!")
+        print("BioBot connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
